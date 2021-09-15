6b209b24fb54ca7a81cf226d1e5a5503e988450c
Matheus Costa <matheus.bionicle@gmail.com>
2021-09-15 15:21:41 -0300

    add post method

--- UserAPI/Controllers/UserController.cs
+++ UserAPI/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using UserAPI.Models;
+using System.Threading.Tasks;
 
 
 namespace UserAPI.Controllers
@@ -26,6 +27,44 @@ namespace UserAPI.Controllers
             return _userContext.Users.ToList();
         }
 
+        [HttpGet]
+        [Route("getbyid/{id}")]
+        public ActionResult<User> GetById(int? id)
+        {
+            if(id <= 0)
+            {
+                return NotFound("User id must be higher than zero.");
+            }
+
+            User user = _userContext.Users.FirstOrDefault(u => u.UserId == id);
+
+            if(user == null)
+            {
+                return NotFound("User not found!");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody]User user)
+        {
+            if(user == null)
+            {
+                return NotFound("User data is not supplied");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }   
+
+            await _userContext.Users.AddAsync(user);
+            await _userContext.SaveChangesAsync();
+        
+            return Ok(user);
+        }
+        
 
         ~UserController()
         {
